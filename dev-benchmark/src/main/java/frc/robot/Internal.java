550f5258833436cf379c78607aa5ddb2be69b04e
Ryan Blue <ryanzblue@gmail.com>
2023-09-14 20:59:34 -0400

    Enable formatting

--- dev-benchmark/src/main/java/frc/robot/Internal.java
+++ dev-benchmark/src/main/java/frc/robot/Internal.java
@@ -1,51 +1,52 @@
 package frc.robot;
 
-import autolog.Logged;
 import autolog.AutoLog.BothLog;
-import autolog.AutoLog.DataLog;
-import autolog.AutoLog.NTLog;
+import autolog.Logged;
 import io.github.oblarg.oblog.Loggable;
 import io.github.oblarg.oblog.annotations.Log;
 
 public class Internal implements Logged, Loggable {
 
-    private int calls = 0;
-    private String name;
-    private String showName;
-    public Internal(String name){
-        this.name = name + "asdfghjklqwertyuiopzxcvbnm,qwertyuio";
-        showName = this.name;
-    };
-    @Override
-    public String getPath() {
-        // TODO Auto-generated method stub
-        return name;
-    }
+  private int calls = 0;
+  private String name;
+  private String showName;
 
-    @Override
-    public String configureLogName() {
-        // TODO Auto-generated method stub
-        return name;
-    }
-    // @Log
-    // @NTLog
-    // public String name="name";
-    //@Log
-    //@DataLog
-    public String getName() {
-        calls++;
-        return showName;
+  public Internal(String name) {
+    this.name = name + "asdfghjklqwertyuiopzxcvbnm,qwertyuio";
+    showName = this.name;
+  }
+  ;
 
-    }
+  @Override
+  public String getPath() {
+    // TODO Auto-generated method stub
+    return name;
+  }
 
-    @Log
-    @BothLog
-    public double getNumber() {
-        calls++;
-        return calls + calls/1000.0;
-    }
+  @Override
+  public String configureLogName() {
+    // TODO Auto-generated method stub
+    return name;
+  }
 
-    public void update() {
-        showName = name + calls;
-    }
+  // @Log
+  // @NTLog
+  // public String name="name";
+  // @Log
+  // @DataLog
+  public String getName() {
+    calls++;
+    return showName;
+  }
+
+  @Log
+  @BothLog
+  public double getNumber() {
+    calls++;
+    return calls + calls / 1000.0;
+  }
+
+  public void update() {
+    showName = name + calls;
+  }
 }
